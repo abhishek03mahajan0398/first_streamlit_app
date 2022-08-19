18f9771fe3f913ad3077d08f7a50519a646d0d1e
Abhishek Mahajan <111556287+abhishek03mahajan0398@users.noreply.github.com>
2022-08-19 11:52:48 +0530

    Update streamlit_app.py
    type all the required details using header and text extension

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,6 @@ streamlit.title('My parents new healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-Range Egg'
 
