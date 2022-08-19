4fc383f1e787a4fea2c9fa55f6dece83fb8f07e9
Abhishek Mahajan <111556287+abhishek03mahajan0398@users.noreply.github.com>
2022-08-19 11:53:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled Free-Range Egg'
+streamlit.text('Hard-Boiled Free-Range Egg')
 
