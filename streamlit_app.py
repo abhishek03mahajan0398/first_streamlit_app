6ba3038830524e8876d0d12417376e3e0a5f454e
Abhishek Mahajan <111556287+abhishek03mahajan0398@users.noreply.github.com>
2022-08-25 16:57:38 +0530

    Update streamlit_app.py
    imported pandas as pd and forgot this to mentioned in the 34th line

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
