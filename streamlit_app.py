aa4d82eeaccc0a85bb5f6c2888d5e6bf365f574c
Abhishek Mahajan <111556287+abhishek03mahajan0398@users.noreply.github.com>
2022-08-26 12:00:07 +0530

    Update streamlit_app.py
    edit code in the 32th line and what fruit you would like to add

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+fruit_choice = streamlit.text_input('What fruit would you like like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
 
 import requests
